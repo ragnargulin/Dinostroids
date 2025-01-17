dbcc13877796184cb6a638d46e77f8c7643d9300
Omar-Alme <omar.almehdar17@gmail.com>
2025-01-17 10:06:47 +0100

    assets

--- src/MainMenu.ts
+++ src/MainMenu.ts
@@ -1,6 +1,14 @@
 class MainMenu implements IScene {
+    // private startGameButton: Button;
+    // private aboutButton: Button;
+    // private howToPlayButton: Button;
+    // private scoreboardButton: Button;
+    private menuBackground: p5.Image;
+
     constructor() {
+        this.menuBackground = loadImage("./assets/images/menuBackground.jpg");
         console.log('MainMenu created');
+
     }
 
     update(): void {
@@ -10,10 +18,10 @@ class MainMenu implements IScene {
 
     draw(): void {
         // Implement logic for rendering the MainMenu scene.
-        background('pink');
-        createButton('About').position(19, 19);
-        createButton('How to play').position(19, 40);
-        createButton('Scoreboard').position(19, 60);
-        createButton('Start game').position(19, 80);
+        image(this.menuBackground,0,0,width,height);
+        // createButton('About').position(19, 19);
+        // createButton('How to play').position(19, 40);
+        // createButton('Scoreboard').position(19, 60);
+        // createButton('Start game').position(19, 80);
     }
 }

da14aadd093cecb4516fc48f396dc7a588184fd6
kaspvik <kasper.vikstrom@medieinstituet.se>
2025-01-23 12:12:44 +0100

    -Spawntimer for the astroids insertet on the gameboard
    - Small fixes with powerups and astroids with David under a live share session

--- src/GameBoard.ts
+++ src/GameBoard.ts
@@ -1,83 +1,109 @@
 class GameBoard implements IScene {
+  private dinoStroids: IChangeableScene;
+  private score: number = 999;
+  private lives: number = 5;
+  private backgroundImage: p5.Image;
+  private heartImage: p5.Image;
 
-    private score: number = 999;
-    private lives: number = 5;
-    private backgroundImage: p5.Image;
-    private heartImage: p5.Image;
+  private menuButton: Button;
 
-    private menuButton: Button;
+  private moveableObject: MoveableObject[];
+  private astroSpawnTimer: number;
 
-    private moveableObject: MoveableObject[];
+  constructor(dinoStroids: IChangeableScene) {
+    this.dinoStroids = dinoStroids;
 
-    constructor(dinoStroids: IChangeableScene) {
-        this.dinoStroids = dinoStroids;
+    this.backgroundImage = imageAssets.background;
+    this.heartImage = imageAssets.hearts;
 
-        this.backgroundImage = imageAssets.background;
-        this.heartImage = imageAssets.hearts;
+    this.menuButton = new Button(
+      "MENU",
+      createVector(width * 0.1, height * 0.06),
+      100,
+      40,
+      "maroon"
+    );
 
-        this.menuButton = new Button("MENU", createVector(width * 0.1, height * 0.06), 100, 40, "maroon");
+    this.moveableObject = [new Player()];
+    this.astroSpawnTimer = 0;
+  }
 
-        this.moveableObject = [new Player()];
+  public update(): void {
+    if (this.menuButton.isClicked()) {
+      console.log("Menu button clicked. (Add code to open a menu/popup here.)");
+      this.dinoStroids.changeActiveScene(new InGameMenuPopup(this.dinoStroids));
+    }
+    for (const gameObject of this.moveableObject) {
+      gameObject.update();
     }
 
-    public update(): void {
-        if (this.menuButton.isClicked()) {
-            console.log("Menu button clicked. (Add code to open a menu/popup here.)");
-            this.dinoStroids.changeActiveScene(new InGameMenuPopup(this.dinoStroids));
-        }
-        for (const gameObject of this.moveableObject) {
-            gameObject.update();
-          }
-      
+    this.spawnAstro();
+  }
 
+  private spawnAstro() {
+    if (this.astroSpawnTimer <= 0) {
+      // const index = floor(random(0, 3));
+      // const asteroids = [new BigAsteroid(), new RegularAstoroid(), new SuperAsteroid()]
+      //   this.moveableObject.push(asteroids[index]);
+      this.moveableObject.push(new RegularAsteroid());
+      this.astroSpawnTimer = 400;
     }
 
-    public draw(): void {
-        imageMode(CORNER);
-        image(this.backgroundImage, 0, 0, width, height);
+    this.astroSpawnTimer -= deltaTime;
+  }
 
-        this.menuButton.draw();
+  public draw(): void {
+    imageMode(CORNER);
+    image(this.backgroundImage, 0, 0, width, height);
 
-        this.drawPlayerInfo();
-
-        this.drawLives();
-
-        //KEVIN
-        for (const gameObject of this.moveableObject) {
-            gameObject.draw();
-          }
+    for (const gameObject of this.moveableObject) {
+      gameObject.draw();
     }
 
-    private drawPlayerInfo(): void {
-        push();
-        fill("white");
-        textAlign(CENTER, TOP);
-        textFont("Pixelify Sans", 24);
-        textStyle(BOLD);
+    this.menuButton.draw();
 
-        const playerInfoX = width * 0.5;
-        const playerInfoY = height * 0.03;
+    this.drawPlayerInfo();
 
-        text(`Score: ${this.score}`, playerInfoX, playerInfoY);
-        pop();
+    this.drawLives();
+
+    //KEVIN
+  }
+
+  private drawPlayerInfo(): void {
+    push();
+    fill("white");
+    textAlign(CENTER, TOP);
+    textFont("Pixelify Sans", 24);
+    textStyle(BOLD);
+
+    const playerInfoX = width * 0.5;
+    const playerInfoY = height * 0.03;
+
+    text(`Score: ${this.score}`, playerInfoX, playerInfoY);
+    pop();
+  }
+
+  private drawLives(): void {
+    push();
+    imageMode(CORNER);
+
+    const heartWidth = 35;
+    const heartHeight = 30;
+    const spacing = 5;
+
+    let heartPositionX = width * 0.9;
+    let heartPositionY = height * 0.02;
+
+    for (let i = 0; i < this.lives; i++) {
+      image(
+        this.heartImage,
+        heartPositionX - i * (heartWidth + spacing),
+        heartPositionY,
+        heartWidth,
+        heartHeight
+      );
     }
 
-    private drawLives(): void {
-        push();
-        imageMode(CORNER);
-
-        const heartWidth = 35;
-        const heartHeight = 30;
-        const spacing = 5;
-
-        let heartPositionX = width * 0.9;
-        let heartPositionY = height * 0.02;
-
-        for (let i = 0; i < this.lives; i++) {
-            image(this.heartImage, heartPositionX - i * (heartWidth + spacing), heartPositionY, heartWidth, heartHeight);
-        }
-
-        pop();
-    }
-
+    pop();
+  }
 }

529ae809efbb13feb25605e3b499faeecd39143f
Kind606 <kevin.hellgren@hotmail.com>
2025-01-28 13:17:00 +0100

    Fixed laser?

--- src/entities/Lazer.ts
+++ src/entities/Lazer.ts
@@ -2,4 +2,8 @@ class Laser extends MoveableObject {
     constructor(x: number, y: number, image: p5.Image) {
       super(x, y, 20, 60, 0, -10, image); 
     }  
+
+    public isOffCanvas(): boolean {
+      return this.position.y < -150 || this.position.y > height;
+    }
 }

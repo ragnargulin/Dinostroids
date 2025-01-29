6876f3bd31dd01a6a10a08ee813ad342e25650d1
Omar-Alme <omar.almehdar17@gmail.com>
2025-01-29 21:56:38 +0100

    revise collision system, fix split astroid to one hit

--- src/CollisionSystem.ts
+++ src/CollisionSystem.ts
@@ -25,59 +25,69 @@ class CollisionSystem {
         }
     ): void {
         for (const obj of moveableObjects) {
+            // Player collides with an Asteroid
             if (obj instanceof RegularAsteroid || obj instanceof BigAsteroid) {
                 if (player.collidesWith(obj)) {
                     console.log("Player collided with asteroid!");
                     if (player.isShieldActive) {
                         onCollision.removeObject(obj);
-                        console.log("Shield took the damage!");
+                        console.log("Shield took the damage from the asteroid!");
                         continue;
                     }
+                    // If no shield, remove the asteroid and damage the player
                     onCollision.removeObject(obj);
                     onCollision.decreaseLives(1);
                     soundeffects.playerHit.play();
                 }
+                // Player collides with a SuperAstro
             } else if (obj instanceof SuperAstro) {
                 if (player.collidesWith(obj)) {
                     console.log("Player collided with SuperAstro!");
                     if (player.isShieldActive) {
                         onCollision.removeObject(obj);
-                        console.log("Shield took the damage!");
+                        console.log("Shield took the damage from SuperAstro!");
                         continue;
                     }
+                    // If no shield, remove SuperAstro and deal heavy damage
                     onCollision.removeObject(obj);
                     onCollision.decreaseLives(4);
                     soundeffects.playerHit.play();
                 }
+
+                // Player collides with a Power-up
             } else if (obj instanceof Heart || obj instanceof Sheild || obj instanceof SuperLaser) {
+                // Only remove the power-up if the player actually collides with it
                 if (player.collidesWith(obj)) {
                     if (obj instanceof Sheild) {
-                        console.log("Dino picked up shield");
+                        console.log("Player picked up Shield");
                         player.activateShield(5000, imageAssets.dinoWithSheild);
                         soundeffects.powerupSound.play();
                     } else if (obj instanceof Heart) {
-                        console.log("Player picked up a heart");
-                        onCollision.decreaseLives(-1); // Increase lives by 1
+                        console.log("Player picked up a Heart");
+                        onCollision.decreaseLives(-1);
                     } else if (obj instanceof SuperLaser) {
                         console.log("Player picked up Super Laser");
                         soundeffects.powerupSound.play();
-                        onCollision.removeObject(obj);
                     }
+                    onCollision.removeObject(obj);
                 }
-                onCollision.removeObject(obj);
             }
         }
     }
-  
+
+    /**
+     * Checks collisions between Lasers and Asteroids (Regular, Big, SuperAstro).
+     * If a laser hits an asteroid, remove laser; handle asteroid destruction/splitting.
+     */
     private checkLaserCollisions(
-            moveableObjects: MoveableObject[],
-            onCollision: {
-                removeObject: (obj: MoveableObject) => void;
-                addObject: (obj: MoveableObject) => void;
-                increaseScore: (amount: number) => void;
-                addExplosion: (position: p5.Vector) => void;
-            }
-        ): void {
+        moveableObjects: MoveableObject[],
+        onCollision: {
+            removeObject: (obj: MoveableObject) => void;
+            addObject: (obj: MoveableObject) => void;
+            increaseScore: (amount: number) => void;
+            addExplosion: (position: p5.Vector) => void;
+        }
+    ): void {
         for (const laser of moveableObjects) {
             if (laser instanceof Laser) {
                 for (const asteroid of moveableObjects) {
@@ -88,8 +98,10 @@ class CollisionSystem {
                     ) {
                         if (laser.collidesWith(asteroid)) {
                             console.log("Laser hit an asteroid!");
+                            // Remove the laser once it collides
                             onCollision.removeObject(laser);
 
+                            // If it's a SuperAstro
                             if (asteroid instanceof SuperAstro) {
                                 if (asteroid.takeDamage()) {
                                     console.log("SuperAstro destroyed!");
@@ -98,14 +110,17 @@ class CollisionSystem {
                                 } else {
                                     console.log(`SuperAstro hit! Hits left: ${asteroid.hitsLeft}`);
                                 }
-                            } else if (asteroid instanceof BigAsteroid) {
+                            }
+                            // If it's a BigAsteroid
+                            else if (asteroid instanceof BigAsteroid) {
                                 const newAsteroids = asteroid.split();
                                 for (const newAsteroid of newAsteroids) {
                                     onCollision.addObject(newAsteroid);
                                 }
                                 onCollision.removeObject(asteroid);
                                 onCollision.increaseScore(15);
-                            } else {
+                            }
+                            else {
                                 onCollision.removeObject(asteroid);
                                 onCollision.increaseScore(5);
                             }
